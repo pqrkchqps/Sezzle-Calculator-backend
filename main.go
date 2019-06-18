e7a944d8ef09b219e70dcc726f285030178cfb83
John A Creps <johnacreps@gmail.com>
2019-06-18 15:54:29 -0500

    added basic subtraction, multiplication, division tests & implementation

--- main.go
+++ main.go
@@ -37,8 +37,68 @@ func addition(w http.ResponseWriter, r *http.Request){
   }
 }
 
+func subtraction(w http.ResponseWriter, r *http.Request){
+  fmt.Println("Endpoint hit: subtraction")
+  u := strings.Split(r.URL.Path, "/")
+  x, err := strconv.ParseFloat(u[3], 64)
+  if err != nil {
+    panic(err)
+  }
+  y, err := strconv.ParseFloat(u[4], 64)
+  if err != nil {
+    panic(err)
+  }
+  calc := x - y
+  returnVal := &Calculation{Op: "-", X: x, Y: y, Calc: calc}
+  fmt.Println(returnVal)
+  if err := json.NewEncoder(w).Encode(returnVal); err != nil {
+    panic(err)
+  }
+}
+
+func multiplication(w http.ResponseWriter, r *http.Request){
+  fmt.Println("Endpoint hit: multiplication")
+  u := strings.Split(r.URL.Path, "/")
+  x, err := strconv.ParseFloat(u[3], 64)
+  if err != nil {
+    panic(err)
+  }
+  y, err := strconv.ParseFloat(u[4], 64)
+  if err != nil {
+    panic(err)
+  }
+  calc := x * y
+  returnVal := &Calculation{Op: "-", X: x, Y: y, Calc: calc}
+  fmt.Println(returnVal)
+  if err := json.NewEncoder(w).Encode(returnVal); err != nil {
+    panic(err)
+  }
+}
+
+func division(w http.ResponseWriter, r *http.Request){
+  fmt.Println("Endpoint hit: division")
+  u := strings.Split(r.URL.Path, "/")
+  x, err := strconv.ParseFloat(u[3], 64)
+  if err != nil {
+    panic(err)
+  }
+  y, err := strconv.ParseFloat(u[4], 64)
+  if err != nil {
+    panic(err)
+  }
+  calc := x / y
+  returnVal := &Calculation{Op: "-", X: x, Y: y, Calc: calc}
+  fmt.Println(returnVal)
+  if err := json.NewEncoder(w).Encode(returnVal); err != nil {
+    panic(err)
+  }
+}
+
 func handleRequests() {
   http.HandleFunc("/calculate/+/", addition)
+  http.HandleFunc("/calculate/-/", subtraction)
+  http.HandleFunc("/calculate/*/", multiplication)
+  http.HandleFunc("/calculate/d/", division)
   log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
