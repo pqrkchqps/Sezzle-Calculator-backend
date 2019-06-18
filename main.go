6c8f421a3edf6f188b6b0a29e32565b15c9cc642
John A Creps <johnacreps@gmail.com>
2019-06-18 12:46:29 -0500

    created basic api endpoint for addition

--- main.go
+++ main.go
@@ -6,15 +6,35 @@ import(
   "fmt"
   "log"
   "net/http"
+  "strings"
+  "strconv"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request){
-  fmt.Fprintf(w, "Hello, Welcome to the Homepage")
+type Calculation struct {
+  op string
+  x float64
+  y float64
+  calc float64
+}
+
+func addition(w http.ResponseWriter, r *http.Request){
+  u := strings.Split(r.URL.Path, "/")
+  x, err := strconv.ParseFloat(u[2], 64)
+  if err != nil {
+    panic(err)
+  }
+  y, err := strconv.ParseFloat(u[3], 64)
+  if err != nil {
+    panic(err)
+  }
+  calc := x + y
+  returnVal := Calculation{"+", x, y, calc}
+  fmt.Fprintf(w, "Hello, Welcome %s", returnVal)
   fmt.Println("Endpoint hit: homepage")
 }
 
 func handleRequests() {
-  http.HandleFunc("/", homePage)
+  http.HandleFunc("/+/", addition)
   log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
